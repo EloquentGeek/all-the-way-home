b04dee54f867aa92fde85f66334a09f9645ff3ae
Rich Churcher <rich.churcher@gmail.com>
2024-12-21 13:10:00 +1300

    Splash

--- src/screens.rs
+++ src/screens.rs
@@ -1,4 +1,5 @@
 mod loading;
+mod splash;
 mod title;
 
 use bevy::prelude::*;
@@ -7,15 +8,15 @@ pub fn plugin(app: &mut App) {
     app.init_state::<Screen>();
     app.enable_state_scoped_entities::<Screen>();
 
-    app.add_plugins((loading::plugin, title::plugin));
+    app.add_plugins((loading::plugin, splash::plugin, title::plugin));
 }
 
 #[derive(States, Debug, Hash, PartialEq, Eq, Clone, Default)]
 pub enum Screen {
-    #[default]
     Loading,
     // Over,
     // Playing,
-    // Splash,
+    #[default]
+    Splash,
     Title,
 }
