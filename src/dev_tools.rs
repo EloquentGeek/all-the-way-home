9b6148d80bf17ebf4867f402afa5c2b387d2262e
Rich Churcher <rich.churcher@gmail.com>
2024-12-28 10:33:59 +1300

    Dev tools initial

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,3 +1,28 @@
-use bevy::prelude::*;
+use bevy::{
+    dev_tools::{
+        states::log_transitions,
+        ui_debug_overlay::{DebugUiPlugin, UiDebugOptions},
+    },
+    input::common_conditions::{input_just_pressed, input_toggle_active},
+    prelude::*,
+};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
-pub fn plugin(mut app: &mut App) {}
+use crate::screens::Screen;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(Update, log_transitions::<Screen>);
+
+    app.add_plugins(DebugUiPlugin);
+    app.add_plugins(WorldInspectorPlugin::default().run_if(input_toggle_active(false, TOGGLE_KEY)));
+    app.add_systems(
+        Update,
+        toggle_debug_ui.run_if(input_just_pressed(TOGGLE_KEY)),
+    );
+}
+
+const TOGGLE_KEY: KeyCode = KeyCode::Backquote;
+
+fn toggle_debug_ui(mut options: ResMut<UiDebugOptions>) {
+    options.toggle();
+}
